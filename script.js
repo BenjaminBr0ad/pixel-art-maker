06d0576ff14a01eec7408073824f2bed46108d21
Benjamin J. Broad <benjamin.broad@colorado.edu>
2018-04-22 22:54:41 -0600

    Fixed bug wherein resetting the grid would execute after a delay by making changes to how color classes are removed and updated across the entire application

--- script.js
+++ script.js
@@ -11,18 +11,7 @@ document.addEventListener("DOMContentLoaded", function(){
 
 // ============[PAINTING/CANVAS FUNCTIONALITY]========= //
 let paint = function(){
-  event.target.classList.remove("red")
-  event.target.classList.remove("blue")
-  event.target.classList.remove("yellow")
-  event.target.classList.remove("green")
-  event.target.classList.remove("purple")
-  event.target.classList.remove("orange")
-  event.target.classList.remove("lite-blue")
-  event.target.classList.remove("pink")
-  event.target.classList.remove("pastel-green")
-  event.target.classList.remove("pastel-yellow")
-  event.target.classList.remove("lite-purple")
-  event.target.classList.remove("lite-orange")
+  event.target.classList.remove(...classList)
   event.target.classList.add(currentColor)
 }
 canvas.addEventListener("click", paint)
@@ -42,20 +31,10 @@ for (let i = 0; i < paletteCollection.length; i++){
 
 // ===============[CURRENT COLOR INDICATOR]============= //
 let currentPanel = document.getElementsByClassName("current")[0]
+let classList = ["red","blue","yellow","green","purple","orange","lite-blue","pink","pastel-green","pastel-yellow","lite-purple","lite-orange"]
 let currentColorUpdate = function(){
   let color = event.target.classList[0]
-  currentPanel.classList.remove("red")
-  currentPanel.classList.remove("blue")
-  currentPanel.classList.remove("yellow")
-  currentPanel.classList.remove("green")
-  currentPanel.classList.remove("purple")
-  currentPanel.classList.remove("orange")
-  currentPanel.classList.remove("lite-blue")
-  currentPanel.classList.remove("pink")
-  currentPanel.classList.remove("pastel-green")
-  currentPanel.classList.remove("pastel-yellow")
-  currentPanel.classList.remove("lite-purple")
-  currentPanel.classList.remove("lite-orange")
+  currentPanel.classList.remove(...classList)
   currentPanel.classList.add(color)
 }
 
@@ -69,18 +48,7 @@ let pixels = document.getElementsByClassName("item")
 let resetButton = document.getElementsByClassName("reset")[0]
 let resetCanvas = function(){
   for (let i = 0; i < pixels.length; i++){
-    pixels[i].classList.remove("red")
-    pixels[i].classList.remove("blue")
-    pixels[i].classList.remove("yellow")
-    pixels[i].classList.remove("green")
-    pixels[i].classList.remove("purple")
-    pixels[i].classList.remove("orange")
-    pixels[i].classList.remove("lite-blue")
-    pixels[i].classList.remove("pink")
-    pixels[i].classList.remove("pastel-green")
-    pixels[i].classList.remove("pastel-yellow")
-    pixels[i].classList.remove("lite-purple")
-    pixels[i].classList.remove("lite-orange")
+    pixels[i].classList.remove(...classList)
   }
 }
 
